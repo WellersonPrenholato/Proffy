24ee351d2a282c7660f170c7d1c62363f1ea4c23
Wellerson Prenholato <wellerson1996@hotmail.com>
2020-08-04 21:17:54 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import Routes from './routes';
+// import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>
-        HELLO WORD!
-      </h1>
-    </div>
+    <Routes />
   );
 }
 
