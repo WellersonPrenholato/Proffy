516ee8765f124346895d1ac9e59cd330b18057d1
Wellerson Prenholato <wellerson1996@hotmail.com>
2020-08-05 21:33:20 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    return response.send('Hello Word');
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+// Query Params: Filtro, paginação e etc.
 
 app.listen(3333);
 
